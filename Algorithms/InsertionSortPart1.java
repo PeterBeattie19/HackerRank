536b1b50d95a2c0418c4059f002b67f6f5402141
Peter Beattie <peterbeattie19@gmail.com>
2017-10-08 16:57:23 +0100

    Create InsertionSortPart1.java

---
+++ Algorithms/InsertionSortPart1.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+
+public class InsertionSortPart1 {
+    
+    
+
+    public static void insertIntoSorted(int[] ar) {
+        // Fill up this function  
+        int e = ar[ar.length-1]; 
+        
+        for(int i = ar.length-2; i>=0; i--) {
+            
+            
+            
+            if(ar[i]>e) {
+                
+                if(i==0) {
+                    ar[i+1] = ar[i]; 
+                    printArray(ar); 
+                    ar[i] = e; 
+                }
+                else 
+                    
+                ar[i+1] = ar[i]; 
+                
+            }
+            
+            else if(ar[i]<e) {
+                ar[i+1] = e; 
+                i = -1;
+            }
+            
+            
+               
+               printArray(ar);
+            
+            
+        }
+    }
+    
+    
+/* Tail starts here */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int[] ar = new int[s];
+        for(int i=0;i<s;i++){
+            ar[i]=in.nextInt(); 
+        }
+        insertIntoSorted(ar);
+    }
+    
+    
+    private static void printArray(int[] ar) {
+        for(int n: ar){
+            System.out.print(n+" ");
+        }
+        System.out.println("");
+    }
+}
+ 
