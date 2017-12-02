d9863014fb7f7fe2ca4651a4e6342b1da9480e96
Peter Beattie <peterbeattie19@gmail.com>
2017-12-02 23:58:31 +0000

    Update CycleDetection.java

--- Data Structures/LinkedLists/CycleDetection.java
+++ Data Structures/LinkedLists/CycleDetection.java
@@ -2,7 +2,7 @@
 Detect a cycle in a linked list. Note that the head pointer may be 'null' if the list is empty.
 
 A Node is defined as: 
-    class Node {
+    public class Node {
         int data;
         Node next;
     }
