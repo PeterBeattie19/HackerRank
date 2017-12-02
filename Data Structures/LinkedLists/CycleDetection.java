3041918e1a2fd99ba2b834141fdb586ecdbb5e04
Peter Beattie <peterbeattie19@gmail.com>
2017-12-02 23:56:25 +0000

    Update CycleDetection.java

--- Data Structures/LinkedLists/CycleDetection.java
+++ Data Structures/LinkedLists/CycleDetection.java
@@ -27,3 +27,29 @@ boolean hasCycle(Node head) {
         
     }
 }
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'null' if the list is empty.
+
+A Node is defined as: 
+    class Node {
+        int data;
+        Node next;
+    }
+*/
+
+boolean hasCycle(Node head) {
+    if(head==null || head.next==null) return false;
+    
+    Node prev = head;
+    Node current = head; 
+    
+    while(current != null && current.next != null){
+        if(prev == current.next.next) return true;
+        
+        prev = prev.next;
+        current = current.next.next;
+            
+        
+    }
+    return false;
+}
