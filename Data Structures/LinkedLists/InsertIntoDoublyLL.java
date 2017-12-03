97b30f39ab978d5368099057b1d8056f377cb908
Peter Beattie <peterbeattie19@gmail.com>
2017-12-03 17:37:18 +0000

    Update InsertIntoDoublyLL.java

--- Data Structures/LinkedLists/InsertIntoDoublyLL.java
+++ Data Structures/LinkedLists/InsertIntoDoublyLL.java
@@ -47,4 +47,56 @@ Node SortedInsert(Node head,int data) {
     return head; 
     
   
+}/*
+  Insert Node at the end of a linked list 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+     Node prev;
+  }
+*/
+
+Node SortedInsert(Node head, int data) {
+    Node insert = new Node();
+    insert.data = data;
+
+    // List is empty
+    if (head == null) {
+        head = insert;
+    }
+    // Insert front
+    else if (data < head.data) {
+        insert.next = head;
+        head.prev = insert;
+        head = insert;
+    }
+    // Insert middle or end
+    else {
+        Node current = head;
+        // Iterate through list to find insertion point
+        while (current.next != null && current.data < data) {
+            current = current.next;
+        } // Stops at end of list or just after insertion point
+
+        // Insert before current
+        if (data < current.data) {
+            Node previous = current.prev;
+
+            // Create links to and from node before insertion point
+            previous.next = insert;
+            insert.prev = previous;
+            // Create links to and from node after insertion point
+            insert.next = current;
+            current.prev = insert;
+        }
+        // Insert after current at end of list
+        else {
+            current.next = insert;
+            insert.prev = current;
+        }
+    }
+
+    return head;
 }
