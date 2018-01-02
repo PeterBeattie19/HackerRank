b119865c2e9f01331b1d980d9170e95459aa9199
Peter Beattie <peterbeattie19@gmail.com>
2018-01-02 19:40:30 +0000

    Create DrawingBookConclusion.py

---
+++ Algorithms/Implementation/DrawingBookConclusion.py
@@ -0,0 +1,12 @@
+#!/bin/python
+
+import sys
+
+def solve(n, p):
+    # Complete this function
+    return min(p/2,n/2-p/2)
+
+n = int(raw_input().strip())
+p = int(raw_input().strip())
+result = solve(n, p)
+print(result)
