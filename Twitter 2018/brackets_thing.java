d6e8d2f77ab887339d3815ed4a7b37a1b9a11480
Peter Beattie <peterbeattie19@gmail.com>
2018-08-28 15:09:37 +0100

    adding class and main

--- Twitter 2018/brackets_thing.java
+++ Twitter 2018/brackets_thing.java
@@ -1,36 +1,44 @@
-public static boolean isBalanced(String expression) {
-    char openings[] = {'{' , '(' , '['}; 
-    char closings[] = {'}' , ')' , ']'}; 
-    
-    Stack<Character> s = new Stack<Character>(); 
-    
-    for(char c: expression.toCharArray()){
-        if(inArr(c,openings)==true){
-            s.push(c); 
-             
+public class Main{
+
+    public static void main(String[] args) {
+        //Write A Java Good here 
+    }
+
+    public static boolean isBalanced(String expression) {
+        char openings[] = {'{' , '(' , '['}; 
+        char closings[] = {'}' , ')' , ']'}; 
+        
+        Stack<Character> s = new Stack<Character>(); 
+        
+        for(char c: expression.toCharArray()){
+            if(inArr(c,openings)==true){
+                s.push(c); 
+                
+            }
+            
+            else {
+                if(s.isEmpty() || isClosing(s.pop(),c)==false) return false; 
+            }
+
         }
         
-        else {
-            if(s.isEmpty() || isClosing(s.pop(),c)==false) return false; 
-        }
-
+        return s.isEmpty(); 
     }
-    
-    return s.isEmpty(); 
-}
 
-public static boolean inArr(char c, char arr[]){
-    for(char s: arr) if(s==c) return true; 
-    
-    return false; 
-}
+    public static boolean inArr(char c, char arr[]){
+        for(char s: arr) if(s==c) return true; 
+        
+        return false; 
+    }
 
-public static boolean isClosing(char c1, char c2){
-    if(c1=='{' && c2=='}') return true; 
-    
-    else if(c1=='(' && c2==')') return true; 
-    
-    else if(c1=='[' && c2==']') return true; 
-    
-    return false; 
+    public static boolean isClosing(char c1, char c2){
+        if(c1=='{' && c2=='}') return true; 
+        
+        else if(c1=='(' && c2==')') return true; 
+        
+        else if(c1=='[' && c2==']') return true; 
+        
+        return false; 
+    }
+
 }
