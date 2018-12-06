192dfee20f2a5b220861d4b7b12fd8194a5af8ef
Peter Beattie <peterbeattie19@gmail.com>
2018-12-06 11:19:31 +0000

    DONE

--- Algorithms/Implementation/CavityMap.py
+++ Algorithms/Implementation/CavityMap.py
@@ -1,8 +1,8 @@
 n = int(input()) 
 a = [list(input()) for _ in range(n)] 
 b = [list(i) for i in a]
-for i in range(1,n-1):
+    for i in range(1,n-1):
     for j in range(1,n-1):
         if a[i+1][j] < a[i][j] and a[i-1][j] < a[i][j] and a[i][j-1] < a[i][j] and a[i][j+1] < a[i][j]:
             b[i][j] = 'X' 
-print("\n".join(["".join(i) for i in b])) 
+print("\n".join(["".join(i) for i in b]))   
